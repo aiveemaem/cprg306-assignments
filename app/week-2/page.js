ff1101eda95b17d277d5614906369f4afb07f80c
Aivee Madrelejos <122317279+aiveemaem@users.noreply.github.com>
2024-01-16 13:18:59 -0700

    add week-2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,27 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
+
+// import Link from "next/link";
+// export default function StudentInfo() {
+//   return (
+//     <div>
+//       <p>Aivee Mae Madrelejos</p>
+//       <p>
+//         <Link
+//           className="hover:text-blue-500 hover:underline"
+//           href="https://github.com/aiveemaem"
+//         >
+//           https://github.com/aiveemaem
+//         </Link>
+//       </p>
+//     </div>
+//   );
+// }
